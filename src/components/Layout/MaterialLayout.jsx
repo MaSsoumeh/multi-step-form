cddcc9b45186a6352454221e8d89e546bdc4b30a
MaSsoumeh <massoumehzarei@gmail.com>
2021-11-25 22:53:17 +0330

    Create Form Layout

---
+++ src/components/Layout/MaterialLayout.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Paper, CssBaseline } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core/styles';
+import Header from '../Header';
+import Footer from '../Footer';
+import { theme, useStyle } from './styles';
+
+const MaterialLayout = (props) => {
+  const { children } = props;
+  const classes = useStyle();
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline /> <Header />
+      <div className={classes.root}>
+        <Paper className={classes.paper}>{children}</Paper>
+      </div>
+      <Footer />
+    </ThemeProvider>
+  );
+};
+export default MaterialLayout;
