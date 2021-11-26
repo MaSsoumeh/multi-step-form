c5ab4058188033af25e2459510b44a27d771c045
MaSsoumeh <massoumehzarei@gmail.com>
2021-11-26 12:23:50 +0330

    export field components

---
+++ src/components/FormFields/index.js
@@ -0,0 +1,4 @@
+import InputField from './InputField';
+import SelectField from './SelectField';
+import DatePickerField from './DatePickerField';
+export { InputField, SelectField, DatePickerField };
