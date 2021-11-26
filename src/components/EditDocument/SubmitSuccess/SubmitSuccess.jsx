e27e3dc757387c4abc15ece900b37ce97e612505
MaSsoumeh <massoumehzarei@gmail.com>
2021-11-26 12:28:00 +0330

    create submit success form

---
+++ src/components/EditDocument/SubmitSuccess/SubmitSuccess.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+function SubmitSuccess() {
+  return (
+    <React.Fragment>
+      <Typography variant='h6' align='center'>
+        مشخصات شما با موفقیت ثبت شد
+      </Typography>
+    </React.Fragment>
+  );
+}
+
+export default SubmitSuccess;
